b9e8f54df7d323aeca1f02f3fe706ca1e98c19f8
dam19-12 <dam19-12@c16x18.iescierva.net>
2019-11-27 13:09:14 +0100

    atributos

--- jv2019/src/java/modelo/Usuario.java
+++ jv2019/src/java/modelo/Usuario.java
@@ -1,5 +1,26 @@
 package modelo;
 
+import java.util.Date;
+
+
 public class Usuario {
+	
+	private String nif;
+	private String nombre;
+	private String apellidos;
+	private String domicilio;
+	private String correo;
+	private Date fechaNacimiento;
+	private Date fechaAlta;
+	private String claveAcceso;
+	private String rol;
+	
+	
+	
+	public static final String [] ROLES = {"NORMAL", "INVITADO","ADMIN"};
+	
+	
+	
+	
 
 }
